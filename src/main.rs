3be59dc3ae6929205deb29cc589113077521807b
Dmitry Zubarev <mathcrosp@yandex.ru>
2017-08-12 02:27:53 +0500

    Moved portscan code to a separate source file.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,13 @@
 extern crate getopts;
 
+mod portscan;
+
 use getopts::Options;
 use std::env;
-use std::net;
 use std::process;
-use std::str::FromStr;
-use std::sync::mpsc;
-use std::thread;
 
 const NTHREADS: u16 = 4;
 const MAX_PORT: u16 = 65535;
-
-fn handle(tx: mpsc::Sender<u16>, host: net::IpAddr,
-          start_port: u16, end_port: u16, nthreads: u16) {
-    let mut port = start_port + 1;
-
-    loop {
-        match net::TcpStream::connect((host, port)) {
-            Ok(_) => {
-                tx.send(port).unwrap();
-            }
-            Err(_) => {}
-        }
-
-        if end_port - nthreads <= port {
-            break;
-        }
-
-        port += nthreads;
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -82,39 +60,5 @@ fn main() {
         process::exit(1);
     }
 
-    println!("scanning {} from {} to {}...", &host, start_port, end_port);
-
-    let addr: net::IpAddr;
-    match net::IpAddr::from_str(&host) {
-        Ok(value) => {
-            addr = value;
-        }
-        Err(_) => {
-            addr = net::IpAddr::V4(
-                net::Ipv4Addr::new(127, 0, 0, 1)
-            );
-        }
-    };
-
-    let (tx, rx) = mpsc::channel();
-    for i in 0..nthreads {
-        let tx = tx.clone();
-
-        thread::spawn(move || {
-            handle(tx, addr, (start_port + i) as u16, end_port, nthreads);
-        });
-    }
-
-    let mut output = vec![];
-    drop(tx);
-    for port in rx {
-        output.push(port);
-    }
-    output.sort();
-
-    println!("");
-
-    for port in output {
-        println!("{} open", port);
-    }
+    portscan::portscan(host, start_port, end_port, nthreads);
 }
