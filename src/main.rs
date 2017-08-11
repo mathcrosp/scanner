f3a52bdc9d803cfd3b2f4e1dd4fe2a87a351da52
Dmitry Zubarev <mathcrosp@yandex.ru>
2017-08-12 03:21:43 +0500

    Added a cli argument for portscan.

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,11 @@ fn main() {
 
     let mut opts = Options::new();
     opts.optflag("h", "help", "Program description");
+    opts.optflag("p", "portscan", "Run ports scanning");
     opts.optopt("a", "host", "Host to scan ports on", "HOST");
     opts.optopt("e", "eport", "Port to end scanning at", "EPORT");
-    opts.optopt("j", "threads", "Number of threads to run", "THREADS");
     opts.optopt("s", "sport", "Port to start scanning from", "SPORT");
+    opts.optopt("j", "threads", "Number of threads to run", "THREADS");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
@@ -34,31 +35,36 @@ fn main() {
         return;
     }
 
-    let host = match matches.opt_str("host") {
-        Some(value) => value.to_string(),
-        None => "127.0.0.1".to_owned(),
-    };
+    if matches.opt_present("p") {
+        let host = match matches.opt_str("host") {
+            Some(value) => value.to_string(),
+            None => "127.0.0.1".to_owned(),
+        };
 
-    let start_port = match matches.opt_str("sport") {
-        Some(value) => value.parse().expect("start port must be an integer"),
-        None => 1,
-    };
+        let start_port = match matches.opt_str("sport") {
+            Some(value) => value.parse().expect("start port must be an integer"),
+            None => 1,
+        };
 
-    let end_port = match matches.opt_str("eport") {
-        Some(value) => value.parse().expect("end port must be an integer"),
-        None => MAX_PORT,
-    };
+        let end_port = match matches.opt_str("eport") {
+            Some(value) => value.parse().expect("end port must be an integer"),
+            None => MAX_PORT,
+        };
 
-    let nthreads = match matches.opt_str("threads") {
-        Some(value) => value.parse().expect("number of threads must be an \
-                                            integer"),
-        None => NTHREADS,
-    };
+        if start_port > end_port {
+            println!("error: start port should be less than end port");
+            process::exit(1);
+        }
 
-    if start_port > end_port {
-        println!("error: start port should be less than end port");
-        process::exit(1);
+        let nthreads = match matches.opt_str("threads") {
+            Some(value) => value.parse().expect("number of threads must be an \
+                                                integer"),
+            None => NTHREADS,
+        };
+
+        portscan::portscan(host, start_port, end_port, nthreads);
+    } else {
+        println!("Nothing to do, terminating");
+        return;
     }
-
-    portscan::portscan(host, start_port, end_port, nthreads);
 }
